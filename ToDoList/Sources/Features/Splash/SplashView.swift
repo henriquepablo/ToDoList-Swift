a60373a6ff0c270745b914b6a8c63027c0de9d84
henriquepablo <pablo269704@gmail.com>
2025-07-06 12:20:42 -0300

    feat: Add Logo on Splash

--- ToDoList/Sources/Features/Splash/SplashView.swift
+++ ToDoList/Sources/Features/Splash/SplashView.swift
@@ -10,4 +10,37 @@ import UIKit
 
 class SplashView: UIView {
     
+    private let logoImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Logo")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = Colors.gray500
+        
+        addSubview(logoImageView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            logoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 150),
+            logoImageView.heightAnchor.constraint(equalToConstant: 150)
+        ])
+    }
 }
