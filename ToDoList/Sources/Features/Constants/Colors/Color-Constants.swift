5ff06cd61c62b8ef1e314b9ca1cd34473aed5f17
henriquepablo <pablo269704@gmail.com>
2025-07-06 12:00:08 -0300

    chore: add Colors constants

---
+++ ToDoList/Sources/Features/Constants/Colors/Color-Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Color-Constants.swift
+//  ToDoList
+//
+//  Created by pablo henrique on 06/07/25.
+//
+
+import Foundation
+import UIKit
+
+
+public struct Colors {
+    static let blueDark = UIColor(red: 30/255, green: 111/255, blue: 100/255, alpha: 1)
+    static let blue = UIColor(red: 78/255, green: 168/255, blue: 222/255, alpha: 1)
+    static let purpleDark = UIColor(red: 94/255, green: 96/255, blue: 206/255, alpha: 1)
+    static let purple = UIColor(red: 130/255, green: 132/255, blue: 250/255, alpha: 1)
+    
+    static let gray700 = UIColor(red: 13/255, green: 13/255, blue: 13/255, alpha: 1)
+    static let gray600 = UIColor(red: 26/255, green: 26/255, blue: 26/255, alpha: 1)
+    static let gray500 = UIColor(red: 38/255, green: 38/255, blue: 38/255, alpha: 1)
+    static let gray400 = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
+    static let gray300 = UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1)
+    static let gray200 = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
+    static let gray100 = UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)
+    
+    static let danger = UIColor(red: 226/255, green: 88/255, blue: 88/255, alpha: 1)
+}
