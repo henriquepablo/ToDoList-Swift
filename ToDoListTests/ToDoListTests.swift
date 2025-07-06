2c53486f4b04c44bf04c7a5f6e0d054f2da5e21b
henriquepablo <pablo269704@gmail.com>
2025-07-06 11:17:31 -0300

    Initial Commit

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by pablo henrique on 06/07/25.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
